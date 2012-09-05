565c9b6e77e8347df4b0c69f2036f23cedf228df
Damon Snyder <drsnyder@gmail.com>
2012-09-05 07:15:47 -0700

    working with get and set from 3rd party client

--- src/poky/protocol/memcache.clj
+++ src/poky/protocol/memcache.clj
@@ -29,22 +29,15 @@
 
 ;get <key>*\r\n
 ;gets <key>*\r\n
-
-; this doesn't work as expected (for this protocol), because the last repeated element will also be
-; delimited. 
-(defcodec KEYS (repeated (string :utf-8 :delimiters [" "]) 
-                         :delimiters ["\r\n" " "]))
-
 (defcodec GET ["get" CR])
 (defcodec GETS ["gets" CR])
 
-(defcodec VALUE ["VALUE" K V V CR CR])
-;VALUE <key> <flags> <bytes> [<cas unique>]\r\n
-;<data block>\r\n
-;...
+;VALUE <key> <flags> <bytes> [<cas unique>]\r\n<data block>\r\n
+;VALUE <key> <flags> <bytes>\r\n<data block>\r\n
+(defcodec VALUE ["VALUE" K V CR CR])
 
-(defcodec END ["END"])
 ;END\r\n
+(defcodec END ["END"])
 
 (defcodec ERRC CR)
 
@@ -53,21 +46,17 @@
   (println (format "body->h: '%s' '%s'" body (first body)))
   (first body))
 
-
-
 (defn h->b [hd] 
   "Called when decoding. Determines how to construct the body."
   (println (format "header h->b: '%s'" hd))
     (case hd
-      "gets" GETS
-      "get"  GET
-      "set" SET
-      "VALUE" VALUE
+      "gets"   GETS
+      "get"    GET
+      "set"    SET
+      "VALUE"  VALUE
       "STORED" STORED
-      "END" END
+      "END"    END
           ERRC))
-
-
 
 (defn b->h 
   "Called when encoding. Determines the header that is generated."
@@ -75,13 +64,9 @@
   (println (format "b->h '%s'" body))
   (first body))
 
-(defn memcache-pre-encode [req]
-  (println (format "memcache-pre-encode '%s'" req))
-  req)
+(defn memcache-pre-encode [req] req)
 
-(defn memcache-post-decode [res]
-  (println (format "memcache-post-decode '%s'" res))
-  res)
+(defn memcache-post-decode [res] res)
 
 (defcodec MEMCACHE (compile-frame 
                      (header CMD h->b b->h)
@@ -110,23 +95,19 @@
 (defn cmd-gets-keys [decoded]
   (clojure.string/split (first (rest decoded)) #" "))
 
-(defn debug-response [ch msg r]
-  (do 
-    (println msg r)
-    (map #(enqueue ch %) r)))
 
-(defn test-handle [ch ci cmd]
+(defn memcache [ch ci cmd]
     (println "Processing command: " (first cmd) " from " ci)
     (condp = (first cmd)
-      "SET" (enqueue ch ["STORED"]) 
-      "GET" (do (enqueue ch ["VALUE" "abc" "123"]) (enqueue ch ["END" ""]))
-      "GETS" (do (enqueue ch ["VALUE" "abc" "123"]) (enqueue ch ["END" ""]))
+      "set" (enqueue ch ["STORED"]) 
+      "get" (do (enqueue ch ["VALUE" "abc" "0" "3" "123"]) (enqueue ch ["END"]))
+      "gets" (do (enqueue ch ["VALUE" "abc" "0" "3" "123"]) (enqueue ch ["END"]))
       (enqueue ch "error")))
 
 
 (defn handler
   [ch ci]
-  (receive-all ch (partial test-handle ch ci)))
+  (receive-all ch (partial memcache ch ci)))
 
-
-;(def s (start-tcp-server handler {:port 10000 :frame MEMCACHE}))
+(defn start-server [port]
+  (start-tcp-server handler {:port port :frame MEMCACHE}))
