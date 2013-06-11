8d6d04514064ade0a6a8f9006cd8e4bbb82a2fa5
loganlinn <logan.linn@gmail.com>
2013-06-11 16:38:11 -0700

    Add statsd counter for HTTP response code

--- src/poky/system.clj
+++ src/poky/system.clj
@@ -1,5 +1,6 @@
 (ns poky.system
   (:require [environ.core :refer [env]]
+            [ring.middleware.statsd :as statsd]
             [clojure.tools.cli :as cli]))
 
 (defprotocol SystemAPI
@@ -25,15 +26,23 @@
 
 (defn cli-runner [store app & args]
   (let [[opts args banner] (cli/cli args
-                           ["-p" "--port" "Listen on this port" :default 8080 :parse-fn #(Integer. %)]
-                           ["-d" "--dsn" "Database DSN" :default (env :database-url "")])
-        port          (:port opts)
-        dsn           (:dsn opts)]
+                                    ["-p" "--port" "Listen on this port" :default 8080 :parse-fn #(Integer. %)]
+                                    ["-d" "--dsn" "Database DSN" :default (env :database-url "")]
+                                    ["--statsd-host" "Statsd host:port" :default (env :statsd-host)])
+        port (:port opts)
+        dsn (:dsn opts)]
 
     (when (empty? dsn)
       (println "--dsn is required.")
       (println banner)
       (System/exit 1))
+
+    (when-let [statsd-host (:statsd-host opts)]
+      (let [[host port] (clojure.string/split statsd-host #":")
+            port (Integer/parseInt port)]
+        (when (and host port)
+          (println "Sending statsd metrics to" statsd-host)
+          (statsd/setup! host port))))
 
     (println (format "Starting up on port %d." port))
     (start (create-system (store dsn) app) port)))
