96493e1a636c83e40d1c546f00ae112460425d95
loganlinn <logan.linn@gmail.com>
2013-06-29 17:21:53 -0700

    Add integration tests for mget and mset

--- test/poky/kv/jdbc/util_test.clj
+++ test/poky/kv/jdbc/util_test.clj
@@ -1,6 +1,7 @@
 (ns poky.kv.jdbc.util-test
   (:require [poky.kv.jdbc.util :refer :all]
             [poky.kv.core :as kv]
+            [poky.util :as util]
             [environ.core :refer [env]]
             [clj-time.core :as t]
             [clj-time.coerce :as tc]
@@ -20,25 +21,67 @@
                                         ?form
                                         (close-connection @@S)))]
   (facts :integration :jdbc-set
-         (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
-         (jdbc-set @@S bucket "key" "value") => (contains {:result "updated"})
-         (jdbc-set @@S bucket "key" "value"
-                   (tc/to-sql-date (t/plus (t/now) (t/days 1)))) => (contains {:result "updated"})
+    (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
+    (jdbc-set @@S bucket "key" "value") => (contains {:result "updated"})
+    (jdbc-set @@S bucket "key" "value"
+              (tc/to-sql-date (t/plus (t/now) (t/days 1)))) => (contains {:result "updated"})
 
-         ; unconditionally accept a set without a modified
-         (jdbc-set @@S bucket "key" "value") => (contains {:result "updated"})
+    ; unconditionally accept a set without a modified
+    (jdbc-set @@S bucket "key" "value") => (contains {:result "updated"})
 
-         ; the modified_at should be 1 day in the future. this should be
-         ; rejected
-         (jdbc-set @@S bucket "key" "value"
-                   (tc/to-sql-date (t/now))) => (contains {:result "rejected"}))
+    ; the modified_at should be 1 day in the future. this should be
+    ; rejected
+    (jdbc-set @@S bucket "key" "value"
+              (tc/to-sql-date (t/now))) => (contains {:result "rejected"}))
 
   (facts :integration :jdbc-get
-         (jdbc-get @@S bucket "key") => falsey
-         (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
-         (jdbc-get @@S bucket "key") => (contains {:key "key" :data "value"}))
+    (jdbc-get @@S bucket "key") => falsey
+    (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
+    (jdbc-get @@S bucket "key") => (contains {:key "key" :data "value"}))
 
   (facts :integration :jdbc-delete
-         (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
-         (jdbc-delete @@S bucket "key") => '(1)
-         (jdbc-delete @@S bucket "key") => '(0)))
+    (jdbc-set @@S bucket "key" "value") => (contains {:result "inserted"})
+    (jdbc-delete @@S bucket "key") => '(1)
+    (jdbc-delete @@S bucket "key") => '(0))
+
+
+  (with-state-changes [(around :facts (do
+                                        (jdbc-set @@S bucket "key1" "value1" (tc/to-sql-date (t/now)))
+                                        (jdbc-set @@S bucket "key2" "value2" (tc/to-sql-date (t/minus (t/now) (t/minutes 5))))
+                                        ?form
+                                        (jdbc-delete @@S bucket "key1")
+                                        (jdbc-delete @@S bucket "key2")))]
+    (facts :integration :jdbc-mget
+      (fact "basic mget"
+        (jdbc-mget @@S bucket [{:key "key1"} {:key "key2"}])
+        => (just
+             (contains {:key "key1"})
+             (contains {:key "key2"})
+             :in-any-order))
+
+      (facts "empty results"
+        (jdbc-mget @@S bucket []) => empty?
+        (jdbc-mget @@S bucket [{}]) => empty?
+        (jdbc-mget @@S bucket [{:key "unknown-key"}]) => empty?)
+
+      (facts "modified_at"
+        (let [key1-ts (-> (jdbc-get @@S bucket "key1") :modified_at)
+              key1-ts-invalid (java.sql.Timestamp. (+ (.getTime key1-ts) 36000))
+              key2-ts (-> (jdbc-get @@S bucket "key2") :modified_at)]
+          (fact "valid"
+            (jdbc-mget @@S bucket [{:key "key1" :modified_at key1-ts}]) => (just (contains {:key "key1"})))
+          (fact "invalid"
+            (jdbc-mget @@S bucket [{:key "key1" :modified_at key1-ts-invalid}]) => empty?)
+          (fact "invalid + valid"
+            (jdbc-mget @@S bucket [{:key "key1" :modified_at key1-ts-invalid}
+                                   {:key "key2" :modified_at key2-ts}]) => (just (contains {:key "key2"})))))))
+
+    (facts :integration :jdbc-mset
+      (fact "single set"
+        (jdbc-mset @@S bucket [{:key "key1" :data "data1"}]) => (just "inserted"))
+      (fact "multi set"
+        (let [key1-ts (util/http-date->Timestamp "Sat, 29 Jun 2013 22:43:43 GMT")]
+         (jdbc-mset @@S bucket [{:key "key1" :data "data1" :modified_at key1-ts}
+                                 {:key "key2" :data "data2"}]) => (just "inserted" "inserted")
+         (jdbc-get @@S bucket "key1") => (contains {:bucket bucket :key "key1" :data "data1" :modified_at key1-ts})
+         (jdbc-get @@S bucket "key2") => (contains {:bucket bucket :key "key2" :data "data2"})))))
