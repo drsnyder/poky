a1f2bd9123f65a6856a51aff3b29ed7ea6805459
Damon Snyder <drsnyder@gmail.com>
2013-09-03 11:33:59 -0700

    Adding loader script.

--- sql/partitioned/poky.sql
+++ sql/partitioned/poky.sql
@@ -1,7 +1,23 @@
-CREATE TABLE poky (
+DO
+$$
+BEGIN
+
+IF EXISTS (
+    SELECT *
+    FROM   pg_catalog.pg_tables
+    WHERE  schemaname = 'public'
+    AND    tablename  = 'poky'
+    ) THEN
+   RAISE NOTICE 'Table "public"."poky" already exists.';
+ELSE
+  CREATE TABLE poky (
     bucket varchar(256) NOT NULL,
     key varchar(1024) NOT NULL,
     data text,
     created_at timestamptz NOT NULL DEFAULT NOW() CONSTRAINT created_at_utc_check CHECK (EXTRACT(TIMEZONE FROM created_at) = '0'),
-    modified_at timestamptz NOT NULL DEFAULT NOW() CONSTRAINT modified_at_utc_check CHECK (EXTRACT(TIMEZONE FROM modified_at) = '0'),
-);
+    modified_at timestamptz NOT NULL DEFAULT NOW() CONSTRAINT modified_at_utc_check CHECK (EXTRACT(TIMEZONE FROM modified_at) = '0')
+  );
+END IF;
+
+END;
+$$ LANGUAGE plpgsql;
--- sql/partitioned/triggers.sql
+++ sql/partitioned/triggers.sql
@@ -6,6 +6,7 @@ END;
 $$
 LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS poky_parent ON poky;
 CREATE TRIGGER poky_parent BEFORE INSERT OR UPDATE OR DELETE ON poky
         FOR EACH STATEMENT EXECUTE PROCEDURE poky_parent_trigger();
 
