05300b7eabfa6eca62284c0a10478788689b9c79
Damon Snyder <drsnyder@gmail.com>
2013-09-04 10:38:57 -0700

    Adding the constraints after the import.
    /cc @frankfarmer

--- sql/migrations/flat-to-partitioned.sql
+++ sql/migrations/flat-to-partitioned.sql
@@ -1,30 +1,41 @@
+BEGIN;
 SET maintenance_work_mem = '256MB';
 ALTER TABLE poky RENAME TO oldpoky;
 
 \i sql/partitioned/poky.sql
-
 \i sql/shared/stored_procedures.sql
-\i sql/shared/triggers.sql
-
 \i sql/partitioned/stored_procedures.sql
-\i sql/partitioned/triggers.sql
+
+ALTER TABLE poky DROP CONSTRAINT IF EXISTS modified_at_utc_check;
+ALTER TABLE poky DROP CONSTRAINT IF EXISTS created_at_utc_check;
 
 DO
 $$
 DECLARE
   _bucket TEXT;
+  _inserted INTEGER;
 BEGIN
 
     FOR _bucket IN SELECT DISTINCT bucket FROM oldpoky
     LOOP
       RAISE NOTICE 'Creating bucket %', _bucket;
-      PERFORM create_bucket_partition(_bucket);
-      RAISE NOTICE 'Migrating data...';
-      PERFORM format('INSERT INTO poky_%I SELECT * FROM oldpoky WHERE bucket = %L', _bucket, _bucket);
+      EXECUTE create_bucket_partition(_bucket, false);
+
+      RAISE NOTICE 'Migrating data for bucket %...', _bucket;
+      EXECUTE format('INSERT INTO poky_%I SELECT * FROM oldpoky WHERE bucket = %L', _bucket, _bucket);
+      GET DIAGNOSTICS _inserted = ROW_COUNT;
+      RAISE NOTICE 'Inserted % rows for bucket %', _inserted, _bucket;
+
+      RAISE NOTICE 'Adding check bucket = %', _bucket;
+      EXECUTE format('ALTER TABLE poky_%I ADD CONSTRAINT poky_%I_bucket_check CHECK (bucket = %L)', _bucket, _bucket, _bucket);
     END LOOP;
 
 END;
 $$ LANGUAGE plpgsql;
 
+\i sql/shared/triggers.sql
 
+\i sql/partitioned/triggers.sql
+
+COMMIT;
 --- remove oldpoky
--- sql/partitioned/stored_procedures.sql
+++ sql/partitioned/stored_procedures.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION create_bucket_partition(_bucket text) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION create_bucket_partition(_bucket text, _add_check BOOLEAN DEFAULT TRUE) RETURNS VOID AS
 $$
 DECLARE
   _child RECORD;
@@ -17,13 +17,17 @@ BEGIN
   ELSE
     EXECUTE format(
       'CREATE TABLE poky_%I (
-        PRIMARY KEY (key),
-        CHECK (bucket::text = %L)
+        PRIMARY KEY (key)
       ) INHERITS (poky)',
-      _bucket, _bucket);
+      _bucket);
 
     EXECUTE format('CREATE TRIGGER poky_%I_only_if_unmodified_since BEFORE UPDATE ON poky_%I
             FOR EACH ROW EXECUTE PROCEDURE only_if_unmodified_since()', _bucket, _bucket);
+
+    IF (_add_check) THEN
+      EXECUTE format('ALTER TABLE poky_%I ADD CONSTRAINT poky_%I_bucket_check CHECK (bucket = %L)', _bucket, _bucket, _bucket);
+    END IF;
+
   END IF;
 
 END
